9dfb4ec83d1029ca31921200d76b5c1295093c35
Tarek Wahab <tarek@wahab.me.uk>
2017-03-01 14:26:19 +0000

    Add first component

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var open = require('gulp-open');	// open a url in a browser
 var browserify = require('browserify');	// bundles js
 var reactify = require('reactify');	// transforms react jsx to js
 var source = require('vinyl-source-stream');	// use conventional text streams with gulp
+var concat = require('gulp-concat');	// concatenates files
+var lint = require(('gulp-eslint'));	// lint js, including jsx
 
 var config = {
 	port: 9005,
@@ -13,6 +15,10 @@ var config = {
 	paths: {
 		html: './src/*.html',
 		js: './src/**/*.js',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
 		dist: './dist',
 		mainJs: './src/main.js'
 	}
@@ -49,9 +55,21 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(config.paths.html, ['html']);
-	gulp.watch(config.paths.js, ['js']);
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json' }))
+		.pipe(lint.format());
+});
+
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-'use strict';
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
 
-var App = console.log('Hello world from Browserify');
-
-module.export = App;
+React.render(<Home />, document.getElementById('app'));
