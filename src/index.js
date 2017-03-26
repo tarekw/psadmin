b5f4f11cd51e40ab776e4170e46e9bec5036faab
tarekw <tarek@wahab.me.uk>
2017-03-26 23:01:28 +0100

    Adding routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Routes, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; //Webpack imports styles tools
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render (
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
